5afe391040e21ae45958d1beb7f9afb44689e901
Juan Alejandro Work <alej1286@gmail.com>
2023-02-23 20:37:08 -0500

    Conf Jest, and REST Client to Test Auth Api

--- tests/routes/authentication/authentication.test.ts
+++ tests/routes/authentication/authentication.test.ts
@@ -14,13 +14,50 @@ describe('Authentication', () => {
   afterAll(async () => {
     await connection.close();
   });
-  it('should login an user', async () => {
+
+  it('1ST LOGOUT REQUEST', async () => {
+    const res = await request(app).delete('/api/authentication').send();
+    expect(res.statusCode).toEqual(403);
+    expect(res.body).toHaveProperty(
+      'message',
+      'Para entrar aquí tienes que iniciar sesión.',
+    );
+  });
+
+  it('1ST TIME CHECK USER INFO', async () => {
+    const res = await request(app).get('/api/authentication').send();
+    expect(res.statusCode).toEqual(200);
+    expect(res.body).toHaveProperty(
+      'message',
+      'Successful connection to backend service',
+    );
+  });
+
+  it('USER LOGIN', async () => {
     const res = await request(app).post('/api/authentication').send({
-      email: 'admin@admin.com',
-      password: 'admin',
+      email: process.env.TEST_USER_EMAIL,
+      password: process.env.TEST_USER_PASSWORD,
     });
     expect(res.statusCode).toEqual(200);
     expect(res.body).toHaveProperty('user');
     expect(res.body).toHaveProperty('message', 'Inicio de sesión correcto.');
+  });
+
+  it('2ND TIME CHECK USER INFO', async () => {
+    const res = await request(app).get('/api/authentication').send();
+    expect(res.statusCode).toEqual(200);
+    expect(res.body).toHaveProperty(
+      'message',
+      'Successful connection to backend service',
+    );
+  });
+
+  it('USER LOGOUT', async () => {
+    const res = await request(app).delete('/api/authentication').send();
+    expect(res.statusCode).toEqual(403);
+    expect(res.body).toHaveProperty(
+      'message',
+      'Para entrar aquí tienes que iniciar sesión.',
+    );
   });
 });
